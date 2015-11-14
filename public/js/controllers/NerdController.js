14f13198b7d6e15cde95be47cbc08367b0549ed9
Tyslan <verschingellorenz@hotmail.com>
2015-11-14 11:15:23 +0100

    console/log controller check removed

--- public/js/controllers/NerdController.js
+++ public/js/controllers/NerdController.js
@@ -7,7 +7,6 @@ function NerdController (NerdService){
     var vmNerd = this;
     vmNerd.tagline = 'Nothing beats a pocket protector!';
     NerdService.get().then(function (response) {
-        console.log(response.data)
         vmNerd.nerds = response.data;
     });
 }
