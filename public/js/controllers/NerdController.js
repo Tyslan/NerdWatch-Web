0e598ebd8c7cb7ba9a565516d6c2741696a8f53b
Tyslan <verschingellorenz@hotmail.com>
2015-11-16 22:33:33 +0100

    Hiding nerds when not logged in

--- public/js/controllers/NerdController.js
+++ public/js/controllers/NerdController.js
@@ -3,10 +3,16 @@ angular
     .module('NerdApp')
     .controller('NerdController', NerdController);
 
-function NerdController (NerdService){
+function NerdController(NerdService, auth) {
     var vmNerd = this;
     vmNerd.tagline = 'Nothing beats a pocket protector!';
-    NerdService.get().then(function (response) {
-        vmNerd.nerds = response.data;
-    });
+
+    vmNerd.isLoggedIn = auth.isLoggedIn;
+
+    if (vmNerd.isLoggedIn) {
+
+        NerdService.get().then(function (response) {
+            vmNerd.nerds = response.data;
+        });
+    }
 }
