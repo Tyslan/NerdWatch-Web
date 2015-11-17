51cc9c8aad8a150bf8a88b6f18254722c21a1d01
Tyslan <verschingellorenz@hotmail.com>
2015-11-17 20:11:26 +0100

    bugfix seeder

--- seeder.js
+++ seeder.js
@@ -3,22 +3,12 @@ var db = require('./config/db');
 //var seeder = require('./Seeder/seeder.js');
 var seeder = require('mongoose-seed');
 var movies = require('./seeds/movies.json')
+var series = require('./seeds/series.json')
 
 // Connect to MongoDB via Mongoose
 seeder.connect(db.url, function () {
 
     // Load Mongoose models
-    seeder.loadModels([
-        './models/movies.js'
-    ]);
-
-    // Clear specified collections
-    seeder.clearModels(['Movie'], function () {
-        // Callback to populate DB once collections have been cleared
-        seeder.populateModels(movies);
-
-    });
-
     seeder.loadModels([
         './models/series.js'
     ]);
@@ -27,6 +17,18 @@ seeder.connect(db.url, function () {
     seeder.clearModels(['Series'], function () {
         // Callback to populate DB once collections have been cleared
         seeder.populateModels(movies);
+
+    });
+
+    // Load Mongoose models
+    seeder.loadModels([
+        './models/movie.js'
+    ]);
+
+    // Clear specified collections
+    seeder.clearModels(['Movie'], function () {
+        // Callback to populate DB once collections have been cleared
+        seeder.populateModels(series);
 
     });
 });
