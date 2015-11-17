1a9f1059be440e86f5088576cd85b6d103fbae02
Tyslan <verschingellorenz@hotmail.com>
2015-11-17 19:36:49 +0100

    Added api routes for series

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,15 @@ var router = express.Router();
 // Model
 var Nerd = require('./../models/nerd');
 var Movie = require('./../models/movie');
+var Serie = require('./../models/serie');
 
 Nerd.methods(['get', 'put', 'post', 'delete']);
 Nerd.register(router, '/nerds');
 
 Movie.methods(['get', 'put', 'post', 'delete']);
 Movie.register(router, '/movies');
+
+Serie.methods(['get', 'put', 'post', 'delete']);
+Serie.register(router, '/series');
 
 module.exports = router;
