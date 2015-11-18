6339655804bf051099ad668ead0666f36b4c6dcd
Tyslan <verschingellorenz@hotmail.com>
2015-11-18 13:39:19 +0100

    minor change seeder

--- seeder.js
+++ seeder.js
@@ -2,9 +2,9 @@ var db = require('./config/db');
 
 //var seeder = require('./Seeder/seeder.js');
 var seeder = require('mongoose-seed');
-var movies = require('./seeds/movies.json')
-var series = require('./seeds/series.json')
-
+var mongoose = require('mongoose');
+var movies = require('./seeds/movies.json');
+var series = require('./seeds/series.json');
 var timeout = 10000;
 
 // Connect to MongoDB via Mongoose
@@ -12,31 +12,19 @@ seeder.connect(db.url, function () {
 
     // Load Mongoose models
     seeder.loadModels([
-        './models/series.js'
-    ]);
-
-    // Clear specified collections
-    seeder.clearModels(['Series'], function () {
-        // Callback to populate DB once collections have been cleared
-        seeder.populateModels(series);
-
-    });
-
-    // Load Mongoose models
-    seeder.loadModels([
+        './models/series.js',
         './models/movie.js'
     ]);
 
     // Clear specified collections
-    seeder.clearModels(['Movie'], function () {
+    seeder.clearModels(['Series', 'Movie'], function () {
         // Callback to populate DB once collections have been cleared
         seeder.populateModels(movies);
+        seeder.populateModels(series);
     });
 });
 
-//hack to close seeder
 setTimeout(function () {
-    console.log("Seeder reached timeout of 10seconds");
-    console.log("Close seeder");
-    process.exit();
+    console.log("Closing seeder");
+    mongoose.disconnect();
 }, timeout);
