2304f28807b1b44850cf1c749bd4e957b60f7c90
Tyslan <verschingellorenz@hotmail.com>
2015-11-28 14:38:08 +0100

    movies upvote API

--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,36 @@ Nerd.methods(['get', 'put', 'post', 'delete']);
 Nerd.register(router, '/nerds');
 
 Movie.methods(['get', 'put', 'post', 'delete']);
+
+Movie.route('upvote', {
+    detail: true,
+    handler: function(req, res, next) {
+        Movie.findOne({_id: req.params.id}).exec(function(err, movie){
+            movie.upvote();
+        });
+    }
+});
+
 Movie.register(router, '/movies');
 
 Series.methods(['get', 'put', 'post', 'delete']);
 Series.register(router, '/series');
+
+//router.param('movie', function (req, res, next, id) {
+//    var query = Movie.findBy(id);
+//
+//    query.exec(function (err, movie) {
+//        if (err) {
+//            return next(err);
+//        }
+//        if (!movie) {
+//            return next(new Error('Can\'t find movie'));
+//        }
+//
+//        req.movie = movie;
+//        return next();
+//    });
+//});
+
 
 module.exports = router;
