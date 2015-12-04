8568aa7393b9c6a2661ed7e6c5d3efe84c97e5b9
Tyslan <verschingellorenz@hotmail.com>
2015-12-04 19:53:35 +0100

    seeder change for distinctContibutors_test

---
+++ test/distinceContributors_test.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var app = require('../server');
+var request = require('supertest');
+
+var agent = request.agent(app);
+
+describe('GET /api/movies', function () {
+    it('should respond with 200 in case of valid request', function (done) {
+        agent.get('/api/distinctContributors')
+            .send()
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var fetchedData = JSON.parse(res.text);
+                expect(fetchedData).to.be.an('array');
+                expect(fetchedData).to.not.empty;
+
+                fetchedData.forEach(function (movie) {
+                    expect(movie).to.have.all.keys('_id', 'description', 'genres', 'storyline',
+                        'title', 'year', 'contributor', 'date', 'upvotes');
+                    expect(movie.genres).to.be.an('array');
+                });
+                done();
+            });
+    });
+});
